d865bc54b0fc6a6253edae9f7d0b4a5036b875f9
Jon Dufty <jon.dufty95@gmail.com>
2021-08-01 11:19:05 +1000

    created basic backend api for testing

--- app/app.js
+++ app/app.js
@@ -1,13 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var apiRouter   = require('./routes/api');
-
 
 var app = express();
 
@@ -20,9 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,31 +1,43 @@
 var express = require('express');
 var apiRouter = express.Router();
 
+const devices = [
+    "rpi-123",
+    "rpi-456"
+];
+
+const sensors = [1901, 2601];
+
 /* GET home page. */
 apiRouter.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 // List Devices
-apiRouter.get('/devices', (req,res) => (
-    res.send('This is a list of devices')
-));
+apiRouter.get('/devices', (req,res) => {
+    var devList = devices;
+    res.status(200).send(body=devList);
+});
 
 
 // List Sensors
 apiRouter.get('/devices/:deviceID/sensors', (req,res) => {
     deviceID = req.params.deviceID;
-    res.send(`This is a list of sensors for device ${deviceID}`);
+    res.status(200).send(body=sensors);
 });
 
 
 // Get Sensor Config
 apiRouter.get('/devices/:deviceID/sensors/:sensorID', (req,res) => (
-    res.send(`This is a config for of sensor ${req.params.sensorID}`)
+    res.status(200).send(`This is a config for of sensor ${req.params.sensorID}`)
 ));
 
 
 // Update Sensor Config
+apiRouter.post('/devices/:deviceID/sensors/:sensorID', (req, res) => {
+    var body = req.body;
+    res.status(200).send(body=body);
+})
 
 
 // Run device
